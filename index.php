cd4a08aa345ea748ffd5008dd460e6353ba4ec34
Daan Swart <daswart18@outlook.com>
2023-07-15 20:56:15 +0200

    Uitlezen van data uit de database

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include('header.php');
+include('dbcon.php');
 ?>
 <h2>STUDENTEN</h2>
 <table class="table table-hover table-bordered table-striped">
@@ -12,18 +13,28 @@ include('header.php');
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>1</td>
-            <td>Daan</td>
-            <td>Swart</td>
-            <td>31</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Ken</td>
-            <td>Sherry</td>
-            <td>21</td>
-        </tr>
+        <?php
+        try {
+            $query = "SELECT * FROM `studenten`";
+            $result = mysqli_query($conn, $query);
+            $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+            foreach ($result as $row) {
+        ?>
+                <tr>
+                    <td><?php echo $row['id'] ?></td>
+                    <td><?php echo $row['voornaam'] ?></td>
+                    <td><?php echo $row['achternaam'] ?></td>
+                    <td><?php echo $row['leeftijd'] ?></td>
+
+                </tr>
+        <?php
+            }
+        } catch (Exception $e) {
+            $error = $e->getMessage();
+            echo "Query failed: " . $error;
+        }
+        ?>
     </tbody>
 </table>
 <?php
