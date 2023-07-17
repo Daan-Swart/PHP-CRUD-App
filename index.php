3e9d77ff0874081d6a2fb3a8410bdc9706ff5686
Daan Swart <daswart18@outlook.com>
2023-07-17 15:48:19 +0200

    Formulier modal + button

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 include('header.php');
 include('dbcon.php');
 ?>
-<h2>STUDENTEN</h2>
+
+<div class="box1">
+    <h2>STUDENTEN</h2>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Student toevoegen</button>
+</div>
 <table class="table table-hover table-bordered table-striped">
     <thead>
         <tr>
@@ -37,6 +41,38 @@ include('dbcon.php');
         ?>
     </tbody>
 </table>
+
+<!-- Modal -->
+<form>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Student toevoegen</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="voornaam">Voornaam</label>
+                    <input type="text" name="voornaam" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="voornaam">Achternaam</label>
+                    <input type="text" name="voornaam" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="voornaam">Leeftijd</label>
+                    <input type="text" name="voornaam" class="form-control">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Sluiten</button>
+                <button type="button" class="btn btn-success">Toevoegen</button>
+            </div>
+        </div>
+    </div>
+</div>
+</form>
 <?php
 include('footer.php');
 ?>
